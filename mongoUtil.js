d14bc6fe0f97b11105c21f1ed0921f605ce1aa6f
Raul Rios <raulabraham.rios@gmail.com>
2019-11-12 20:24:06 -0600

    Functionality mongoUtil
    * Add functionality to the re-use connection

--- mongoUtil.js
+++ mongoUtil.js
@@ -1,16 +1,18 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://admin:admin@cluster0-bseop.mongodb.net/test?retryWrites=true&w=majority";
+var url = "mongodb+srv://admin:admin@cluster0-1iddz.mongodb.net/DB-COTOS?retryWrites=true&w=majority";
 
 var _db;
 
 module.exports = {
-    createConnection: (callBack) => {
-        MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
-            _db = client.db("bedu");
-            return callBack(err);
-        });
+
+    connectToServer: ( callback ) => {
+      MongoClient.connect( url,  { useNewUrlParser: true }, ( err, client ) => {
+        _db  = client.db('DB-COTOS');
+        return callback( err );
+      } );
     },
+  
     getDb: () => {
-        return _db;
+      return _db;
     }
-}
+  };
--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 var express = require('express');
 var app = express();
 // MongoDB
-var ObjectId = require('mongodb').ObjectId;
-var mongoUtil = require( 'mongoUtil' );
-var db = mongoUtil.getDb();
+var mongoUtil = require('./mongoUtil');
 // Security packages
 var cors = require('cors');
 // Settings
@@ -16,9 +14,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/login', (req, res) => {
-    mongoUtil.createConnection((err, client) => {
-        if (err) console.log(err);
-        db.collection("users").findOne({});
+    mongoUtil.connectToServer((err) => {
+        if (err) throw err;
+        let Collection = mongoUtil.getDb().collection('users');
+        let query = { mail: req.body.email, password: req.body.pass };
+        Collection.findOne(query, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+        });
     });
 });
 
